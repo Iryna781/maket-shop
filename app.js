5c680f798998e83ebfa9fcb766cd03b3d33e0f76
Iryna781 <115889892+Iryna781@users.noreply.github.com>
2022-12-05 15:53:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
-// let productsCountEl = document.getElementById("products-count");
+let productsCountEl = document.getElementById("products-count");
 // console.log(productsCountEl);
 
 //add to cart
-// let addCartBtns = document.querySelectorAll(".btn-add");
+let addCartBtns = document.querySelectorAll(".btn-add");
 // console.log(addCartBtns);
 
-// for (let i = 0; i < addCartBtns.length; i++) {
-//   addCartBtns[i].addEventListener("click", function () {
-//     // let prevProductsCount = +productsCountEl.textContent;
-//     // productsCountEl.textContent = prevProductsCount + 1;
-//     productsCountEl.textContent = +productsCountEl.textContent + 1;
-//   });
-// }
+for (let i = 0; i < addToCartBtns.length; i++) {
+  addToCartBtns[i].addEventListener("click", function () {
+    // let prevProductsCount = +productsCountEl.textContent;
+    // productsCountEl.textContent = prevProductsCount + 1;
+    productsCountEl.textContent = +productsCountEl.textContent + 1;
+  });
+}
 
 // //modal
-// let moreDetailsBtns = document.querySelectorAll(".btn-details");
-// let modal = document.querySelector(".modal");
-// let btnClose = document.querySelector(".btn-close");
+let moreDetailsBtns = document.querySelectorAll(".btn-details");
+let modal = document.querySelector(".modal");
+let btnClose = document.querySelector(".btn-close");
 // console.log(btnClose);
 // // console.log(moreDetailsBtns);
 // // console.log(modal);
 
-// moreDetailsBtns.forEach((item) => {
-//   item.addEventListener("click", function () {
-//     modal.classList.add("show");
-//     modal.classList.remove("hide");
-//   });
-// });
+moreDetailsBtns.forEach((item) => {
+  item.addEventListener("click", openModal); 
+});
 
-// btnClose.addEventListener("click", function () {
-//   modal.classList.add("hide");
-//   modal.classList.remove("show");
-// });
+btnClose.addEventListener("click", closeModal); 
 
-// function openModal() {
-//   modal.classList.add("show");
-//   modal.classList.remove("hide");
-// }
-// function closeModal() {
-//   modal.classList.add("hide");
-//   modal.classList.remove("show");
-// }
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
 
-// modal.addEventListener("click", function (e) {
-//   if (e.target === modal) {
-//     closeModal();
-//   }
-// });
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
